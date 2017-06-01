e20d90b5df8012c8db14902aa2c58f8159405a17
Zarian <kredixlabs@gmail.com>
2017-06-01 13:20:02 +0600

    Доделал скачивание фото по id

--- vk_photo.py
+++ vk_photo.py
@@ -4,11 +4,13 @@ import json
 token = '316109efc4cbdeb2997daf111c8fe14d520fc1763473c258a4e58c05959c9f21b6f35e26c3d662d4907a1'
 
 
+# Сохраняем ответ сервера ВК в файл photos_json
 def write_json (data):
     with open('photos_json', 'w') as file:
         json.dump(data, file, indent=2, ensure_ascii=False)
 
 
+# Определяем ориентацию фото (горизонтальное или вертикальное)
 def get_largest(size_dict):
     if size_dict['width'] >= size_dict['height']:
         return size_dict['width']
@@ -16,7 +18,8 @@ def get_largest(size_dict):
         return size_dict['height']
 
 
-def download_photo(url):
+# Сохраняем фото на жесткий диск
+def save_photo(url):
     r = requests.get(url, stream=True)
 
     filename = '1/' + url.split('/')[-1]
@@ -26,17 +29,23 @@ def download_photo(url):
         for chunk in r.iter_content(4096):
             file.write(chunk)
 
-# r = requests.get('https://api.vk.com/method/photos.get',
-#                 params={'owner_id': 169106238,
-#                         'album_id': 'profile',
-#                         'photo_sizes': True,
-#                         'access_token': token})
-# write_json(r.json())
 
-photos = json.load(open('photos_json'))['response']
+# Получаем фото пользователя с максимальным расширением
+def download_photo(id_user):
+    r = requests.get('https://api.vk.com/method/photos.get',
+                     params={'owner_id': id_user,
+                             'album_id': 'profile',
+                             'photo_sizes': True,
+                             'access_token': token})
+    write_json(r.json())
 
-for photo in photos:
-    sizes = photo['sizes']
+    photos = json.load(open('photos_json'))['response']
 
-    max_sizes_url = max(sizes, key=get_largest)['src']
-    download_photo(max_sizes_url)
+    for photo in photos:
+        sizes = photo['sizes']
+
+        max_sizes_url = max(sizes, key=get_largest)['src']
+        save_photo(max_sizes_url)
+
+
+download_photo(169106238)
